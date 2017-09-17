b0073cb3ab5ded1c8198247f7dd2e88054d4afeb
Sergey12S <serg12ius@gmail.com>
2017-09-17 13:11:03 +0300

    checkpoint step 2.0

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
+path1=/home/box/web/ask
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+
+cd $path1
+sudo gunicorn ask.wsgi:application &
