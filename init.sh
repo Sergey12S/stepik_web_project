5beef3df845ca134c7ae6a18d515f3cad8fa8665
Sergey12S <serg12ius@gmail.com>
2017-09-17 17:52:29 +0300

    edit init.sh, delete django.conf

--- init.sh
+++ init.sh
@@ -2,8 +2,9 @@ path1=/home/box/web/ask
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 cd $path1
-sudo gunicorn ask.wsgi:application &
+sudo gunicorn --bind=0.0.0.0:8000 ask.wsgi:application &
